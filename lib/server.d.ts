c1b55eaa67ec71d6c6b13a0ef220b6996f309413
Christy Haragan <christy@christy.haragan@gmail.com>
2015-10-12 11:10:05 +0100

    Update to latest version of markscript

--- lib/server.d.ts
+++ lib/server.d.ts
@@ -19,7 +19,20 @@ export declare class Server {
     private options;
     private services;
     private httpServer;
+    private client;
     getService<T>(name: string): T;
+    callGet<T>(name: string, args?: {
+        [name: string]: string | number | boolean;
+    }): Promise<T>;
+    callPost<T>(name: string, args?: {
+        [name: string]: string | number | boolean;
+    }, body?: string | Object | Buffer | NodeJS.ReadableStream): Promise<T>;
+    callPut<T>(name: string, args?: {
+        [name: string]: string | number | boolean;
+    }, body?: string | Object | Buffer | NodeJS.ReadableStream): Promise<T>;
+    callDelete<T>(name: string, args?: {
+        [name: string]: string | number | boolean;
+    }): Promise<T>;
     constructor(options: RunOptions);
     stop(): void;
     start(): Promise<Server>;
--- src/lib/server.ts
+++ src/lib/server.ts
@@ -1,12 +1,12 @@
 import * as path from 'path'
 import * as io from 'socket.io'
 import * as http from 'http'
+import * as m from 'markscript-core'
 import * as mu from 'markscript-uservices'
 import * as u from 'uservices'
-import * as RxRouter from 'koa-rx-router'
+import {RxRouter} from 'koa-rx-router'
 import {createLocalProxy} from 'uservices-socket.io-server'
-import {createAdminClient} from 'ml-admin'
-import {Client} from 'marklogic'
+import {DatabaseClient, createDatabaseClient} from 'marklogic'
 import * as fs from 'fs'
 
 let koa = require('koa')
@@ -31,13 +31,43 @@ export interface RunOptions {
   fileServerPath?: string
 }
 
+
 export class Server {
   private options: RunOptions
   private services: { [name: string]: any } = {}
   private httpServer: http.Server
+  private client: DatabaseClient
 
   getService<T>(name: string): T {
     return this.services[name]
+  }
+
+  callGet<T>(name: string, args?: { [name: string]: string | number | boolean }): Promise<T> {
+    let self = this
+    return new Promise(function(resolve, reject) {
+      self.client.resources.get(name, args).result(resolve, reject)
+    })
+  }
+
+  callPost<T>(name: string, args?: { [name: string]: string | number | boolean }, body?: string | Object | Buffer | NodeJS.ReadableStream): Promise<T> {
+    let self = this
+    return new Promise(function(resolve, reject) {
+      self.client.resources.post(name, args, body).result(resolve, reject)
+    })
+  }
+
+  callPut<T>(name: string, args?: { [name: string]: string | number | boolean }, body?: string | Object | Buffer | NodeJS.ReadableStream): Promise<T> {
+    let self = this
+    return new Promise(function(resolve, reject) {
+      self.client.resources.put(name, args, body).result(resolve, reject)
+    })
+  }
+
+  callDelete<T>(name: string, args?: { [name: string]: string | number | boolean }): Promise<T> {
+    let self = this
+    return new Promise(function(resolve, reject) {
+      self.client.resources.remove(name, args).result(resolve, reject)
+    })
   }
 
   constructor(options: RunOptions) {
@@ -77,7 +107,7 @@ export class Server {
       let httpServer = http.createServer(fn)
       let ioServer = io(httpServer)
 
-      let mlClient = createAdminClient({
+      self.client = createDatabaseClient({
         port: self.options.database.port,
         host: self.options.database.host,
         password: self.options.database.password,
@@ -87,7 +117,7 @@ export class Server {
 
       u.visitServices(self.options.serviceSpecs, {
         onService: function(service) {
-          let proxy = mu.createRemoteProxy(service, mlClient, router)
+          let proxy = mu.createRemoteProxy(service, self.client, router)
           self.services[service.name] = proxy
           createLocalProxy(ioServer, service, proxy)
         }
